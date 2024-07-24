2cff7981b3a9753f9474be4908e523c32a1d926a
TigerZhu10 <143968664+TigerZhu10@users.noreply.github.com>
2024-07-23 22:09:48 -0700

    Ready for transfer

--- Practice.py
+++ Practice.py
@@ -3,20 +3,32 @@ import pygame
 class BankAccount:
     def __init__(self):
         self.balance = 0
+        self.check_money = False
 
     def deposit(self, cash):
         self.balance += cash
+        self.check_money = True
+        
         
 
-    def withdraw(self):
-        pass
+    def withdraw(self,cash):
+        if cash < self.balance: 
+            self.balance -= cash
+            self.check_money = True
+        else:
+            self.check_money = False
     
     def check_balance(self):
-        return self.balance
+        if self.check_money == True:
+            return self.balance
+        else:
+            return("You are poor")
 
     def transfer(self):
         pass
 
 Tiger_account = BankAccount()
 Tiger_account.deposit(500)
+print(Tiger_account.check_balance())
+Tiger_account.withdraw(200)
 print(Tiger_account.check_balance())
