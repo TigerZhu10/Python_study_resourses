f4e44a3281b6df951b6b4d19f40ab4983dbaa699
TigerZhu10 <143968664+TigerZhu10@users.noreply.github.com>
2024-07-26 21:58:26 -0700

    done

--- Practice.py
+++ Practice.py
@@ -25,7 +25,10 @@ class BankAccount:
             return("You are poor")
 
     def transfer(self, cash, target_account):
-        target_account.balance += cash
+        if self.balance >= cash:
+            target_account.balance += cash
+            self.withdraw(cash)
+
         if target_account.balance > 0:
             target_account.check_money = True
         self.withdraw(cash)
@@ -39,7 +42,7 @@ Tiger_account.deposit(500)
 print(Tiger_account.check_balance())
 Tiger_account.withdraw(200)
 print(Tiger_account.check_balance())
-Tiger_account.transfer(100, Alex_account)
+Tiger_account.transfer(300, Alex_account)
 print(Alex_account.check_balance())
 
 
