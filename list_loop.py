d3f28b95b5f977a185a897042cbf19411c016be9
Tiger Zhu <143968664+TigerZhu10@users.noreply.github.com>
2025-04-14 18:57:44 -0700

    stuff added

---
+++ list_loop.py
@@ -0,0 +1,27 @@
+num = [7,11,3,4]
+for n in num:
+    print(n)
+
+print("-------------------------------")
+num_2 = [7,11,3,4]
+for index, n in enumerate(num_2):
+    print(f"{index}:{n}")
+
+print("-------------------------------")
+num_3 = [[7,11,3,4], [2,6,5,9]]
+for  n in num_3:
+    print(n)
+
+print("-------------------------------")
+num_4 = [[7,11,3,4], 
+        [2,6,5,9]]
+for n in num_4:
+    for j in n:
+        print(j)
+
+print("-------------------------------")
+num_5 = [[7,11,3,4], 
+        [2,6,5,9]]
+for row, n in enumerate(num_5):
+    for column, j in enumerate(n):
+        print(f"row{row}, column{column}:{j}")
--- tile_map/main.py
+++ tile_map/main.py
@@ -1,2 +1,46 @@
 import pygame
 
+pygame.init()
+
+WINDOW_WIDTH = 320
+WINDOW_HEIGHT = 160
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Game")
+
+grass = pygame.image.load("./tile_map/grass.png")
+dirt = pygame.image.load("./tile_map/dirt.png")
+water = pygame.image.load("./tile_map/water.png")
+tile_size = 32
+
+tilemap = [
+    [4,4,4,4,4,4,4,4,4,4],
+    [4,4,4,4,4,4,4,4,4,4],
+    [1,1,4,1,1,1,4,4,1,1],
+    [0,0,2,0,0,0,2,2,0,0],
+    [0,0,2,0,0,0,2,2,0,0],
+]
+
+
+def draw_tilemap():
+    for y, row in enumerate(tilemap):
+        for x, tile in enumerate(row):
+            if tile == 0:
+                screen.blit(dirt, (x * tile_size, y * tile_size))
+            elif tile == 1:
+                screen.blit(grass, (x * tile_size, y * tile_size))
+            elif tile == 2:
+                screen.blit(water, (x * tile_size, y * tile_size))
+
+
+
+game_running = True
+while game_running:
+    for ev in pygame.event.get():
+        if ev.type == pygame.QUIT:
+            game_running = False
+
+    draw_tilemap()
+
+    pygame.display.update()
+pygame.quit
+
