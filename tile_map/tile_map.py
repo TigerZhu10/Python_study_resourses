4887a70147ed1319012ce68ac34ddb1525eeec61
Tiger Zhu <143968664+TigerZhu10@users.noreply.github.com>
2025-04-18 22:06:31 -0700

    tlie map example complete

---
+++ tile_map/tile_map.py
@@ -0,0 +1,66 @@
+import pygame
+
+pygame.init()
+
+WINDOW_WIDTH = 960
+WINDOW_HEIGHT = 640
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Game")
+
+grass = pygame.image.load("./tile_map/grass.png")
+dirt = pygame.image.load("./tile_map/dirt.png")
+water = pygame.image.load("./tile_map/water.png")
+tile_size = 32
+
+tilemap = [
+    [4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4],
+    [4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4],
+    [1,1,1,1,1,1,1,1,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,1,1,1,1],
+    [0,0,0,0,0,0,0,0,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,0,0,0,0],
+    [4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4],
+    [4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4],
+    [4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,1,1,1,1,4,4,4,4,4,4,4,4],
+    [4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,0,0,0,0,4,4,4,4,4,4,4,4],
+    [4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4],
+    [4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4],
+    [4,4,4,4,4,4,4,4,4,4,1,1,1,1,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4],
+    [4,4,4,4,4,4,4,4,4,4,0,0,0,0,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4],
+    [1,1,1,1,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,1,1],
+    [0,0,0,0,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,0,0],
+    [4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4],
+    [4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4],
+    [4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4,4,4,4,4,4,4,4,1,1,1,1,1,1],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,2,2,2,2,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,2,2,2,2,0,0,0,0,0,0],
+]
+
+
+def draw_tilemap():
+    for y, row in enumerate(tilemap):
+        for x, tile in enumerate(row):
+            if tile == 0:
+                screen.blit(dirt, (x * tile_size, y * tile_size))
+            elif tile == 1:
+                screen.blit(grass, (x * tile_size, y * tile_size))
+            elif tile == 2:
+                screen.blit(water, (x * tile_size, y * tile_size))
+
+bg_image = pygame.image.load("./tile_map/background.png")
+bg_image = pygame.transform.scale(bg_image, (WINDOW_WIDTH, WINDOW_HEIGHT))
+
+
+game_running = True
+while game_running:
+    for ev in pygame.event.get():
+        if ev.type == pygame.QUIT:
+            game_running = False
+
+    screen.blit(bg_image, (0, 0))
+    screen.blit(bg_image, (0, 0))
+
+    draw_tilemap()
+
+    pygame.display.update()
+pygame.quit()
+
